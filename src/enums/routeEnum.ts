6bba97962dc557aa9f77a7f2cbd6cca6ecfa432b
erkelost <1256029807@qq.com>
2022-04-30 13:00:23 +0800

    chore(update): enum

--- src/enums/routeEnum.ts
+++ src/enums/routeEnum.ts
@@ -8,3 +8,11 @@ export enum RouteConstant {
   PAGE_LOGIN = 'login',
   PAGE_REDIRECT_NAME = 'redirect'
 }
+export enum EnumThemeAnimateMode {
+  'zoom-fade' = '渐变',
+  'zoom-out' = '闪现',
+  'fade-slide' = '滑动',
+  'fade' = '消退',
+  'fade-bottom' = '底部消退',
+  'fade-scale' = '缩放消退'
+}
