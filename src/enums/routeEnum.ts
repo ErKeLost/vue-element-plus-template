b7f262dcb134c41506a3faf86cd7a1910124ee72
erkelost <1256029807@qq.com>
2022-04-30 13:00:48 +0800

    chore(update): enum

--- src/enums/routeEnum.ts
+++ src/enums/routeEnum.ts
@@ -16,3 +16,25 @@ export enum EnumThemeAnimateMode {
   'fade-bottom' = '底部消退',
   'fade-scale' = '缩放消退'
 }
+export enum EnumLayoutComponentName {
+  basic = 'basic-layout',
+  blank = 'blank-layout'
+}
+
+export enum EnumThemeLayoutMode {
+  'vertical' = '左侧菜单模式',
+  'horizontal' = '顶部菜单模式',
+  'vertical-mix' = '左侧菜单混合模式',
+  'horizontal-mix' = '顶部菜单混合模式'
+}
+
+export enum EnumThemeTabMode {
+  'chrome' = '谷歌风格',
+  'button' = '按钮风格'
+}
+
+export enum EnumThemeHorizontalMenuPosition {
+  'flex-start' = '居左',
+  'center' = '居中',
+  'flex-end' = '居右'
+}
