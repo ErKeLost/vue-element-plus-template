7d1daf042508e82e1e36cd41b17aeb8b10e6f4fe
erkelost <1256029807@qq.com>
2022-04-30 17:15:31 +0800

    fix(eslint): 修复eslint 导致 文件丢失bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,6 @@ module.exports = {
   },
   plugins: ['vue', '@typescript-eslint', 'import', 'prettier'],
   rules: {
-    'no-unused-vars': 'off',
     'vue/script-setup-uses-vars': 'error',
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/no-var-requires': 'off',
