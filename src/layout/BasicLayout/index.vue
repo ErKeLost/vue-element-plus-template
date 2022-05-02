39199dfde5912e9c53408e2b09ec7d7f64772338
erkelost <1256029807@qq.com>
2022-05-02 21:16:47 +0800

    chore(axios): test axios

--- src/layout/BasicLayout/index.vue
+++ src/layout/BasicLayout/index.vue
@@ -56,11 +56,32 @@
       <el-button type="warning" round>Warning</el-button>
       <el-button type="danger" round>Danger</el-button>
     </el-row>
-    <el-carousel indicator-position="outside">
+    <el-carousel :interval="4000" type="card" height="200px">
+      <el-carousel-item v-for="item in 6" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+    <el-carousel
+      :initial-index="5"
+      :interval="40000"
+      type="card"
+      height="400px"
+    >
       <el-carousel-item v-for="item in a" :key="item">
         <img :src="item.pic" />
       </el-carousel-item>
     </el-carousel>
+    <el-select v-model="value" class="m-2" placeholder="Select" size="large">
+      <el-option
+        v-for="item in a"
+        :key="item.value"
+        :label="item.label"
+        :value="item.pic"
+      >
+        <img :src="item.pic" alt="" />
+      </el-option>
+    </el-select>
+    <el-button plain @click="open1"> Closes automatically </el-button>
     <div v-for="i in 100" :key="i">{{ i }}</div>
   </adny-layout>
   <ThemeSetting />
@@ -70,25 +91,58 @@
 import { useAppStore, useThemeStore } from '@/store'
 import { useBasicLayout } from '@/composables'
 import { createAxios } from '@erkelost/axios-ext'
+import axios from 'axios'
+const value = ref('')
 const http = createAxios({
   baseURL: '/api',
   timeout: 5000
 })
 const a = ref<any>(null)
+axios
+  .get('http://123.207.32.32:9001/banner?type=2')
+  .then(function (res) {
+    console.log(res)
+    a.value = res.data.banners
+  })
+  .catch(function (error) {
+    console.log(error)
+  })
 http('/banner?type=2').then((res) => {
   console.log(res)
-  a.value = res.data.banners
+  // a.value = res.data.banners
 })
 
 const app = useAppStore()
 const theme = useThemeStore()
 
 const { mode, siderWidth, siderCollapsedWidth } = useBasicLayout()
+const open1 = () => {
+  ElNotification({
+    title: 'Title',
+    message: h('img', { src: a.value[0].pic })
+  })
+}
 </script>
 
-<style>
+<style scoped>
 body {
   margin: 0;
   padding: 0;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 200px;
+  margin: 0;
+  text-align: center;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
--- types/auto-imports.d.ts
+++ types/auto-imports.d.ts
@@ -36,6 +36,7 @@ declare global {
   const effectScope: typeof import('vue')['effectScope']
   const EffectScope: typeof import('vue')['EffectScope']
   const ElMessageBox: typeof import('element-plus/es')['ElMessageBox']
+  const ElNotification: typeof import('element-plus/es')['ElNotification']
   const extendRef: typeof import('@vueuse/core')['extendRef']
   const getActivePinia: typeof import('pinia')['getActivePinia']
   const getCurrentInstance: typeof import('vue')['getCurrentInstance']
