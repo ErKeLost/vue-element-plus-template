2592ec26b9121f10181247ef54fa7135f4247ba9
erkelost <1256029807@qq.com>
2022-05-06 17:28:24 +0800

    chore(build): test depoly

--- src/views/login/cpns/login-account.vue
+++ src/views/login/cpns/login-account.vue
@@ -7,8 +7,10 @@
         h="screen"
         font="mono"
       >
+        <div>
+          <el-button type="primary" @click="login">登录</el-button>
+        </div>
         <Coordinate label="X" :value="x" />
-        <Coordinate label="Y" :value="y" />
         <Coordinate label="Y" :value="y" />
       </div>
     </div>
@@ -17,4 +19,8 @@
 
 <script lang="ts" setup>
 const { x, y } = useMouse()
+const router = useRouter()
+function login() {
+  router.push('/companyUserManagement')
+}
 </script>
