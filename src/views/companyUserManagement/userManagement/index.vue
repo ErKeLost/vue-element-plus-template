e8c9937409ab5165eb070750296c3ce78d56e361
erkelost <1256029807@qq.com>
2022-05-06 14:36:01 +0800

    chore(test): build test

--- src/views/companyUserManagement/userManagement/index.vue
+++ src/views/companyUserManagement/userManagement/index.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>公司用户管理</div>
-  <el-date-picker
-    v-model="value1"
-    type="datetime"
-    placeholder="Select date and time"
-  />
+  <div>
+    <div>公司用户管理</div>
+    <el-date-picker
+      v-model="value1"
+      type="datetime"
+      placeholder="Select date and time"
+    />
+  </div>
 </template>
 <script setup lang="ts">
 const value1 = ref('')
