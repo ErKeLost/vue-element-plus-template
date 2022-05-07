dddead9cafdd1f75abfea0397a379ab606a3b1a0
erkelost <1256029807@qq.com>
2022-05-08 07:54:19 +0800

    refactor(build): 优化打包体积 减少 百分之30

--- types/auto-imports.d.ts
+++ types/auto-imports.d.ts
@@ -35,7 +35,6 @@ declare global {
   const eagerComputed: typeof import('@vueuse/core')['eagerComputed']
   const effectScope: typeof import('vue')['effectScope']
   const EffectScope: typeof import('vue')['EffectScope']
-  const ElMessageBox: typeof import('element-plus/es')['ElMessageBox']
   const extendRef: typeof import('@vueuse/core')['extendRef']
   const getActivePinia: typeof import('pinia')['getActivePinia']
   const getCurrentInstance: typeof import('vue')['getCurrentInstance']
